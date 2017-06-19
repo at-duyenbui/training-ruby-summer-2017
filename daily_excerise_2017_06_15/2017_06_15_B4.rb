cf8d336b7c09a28715c8e10af5c2efc334ff6274
Duyen Bui <anhduyen1510@gmail.com>
2017-06-19 22:48:00 +0700

    daily report 2017_06_19

--- daily_excerise_2017_06_15/2017_06_15_B4.rb
+++
@@ -1,5 +0,0 @@
-def century_from_year(year)
-	century = 0
-	(year % 100 != 0) ? century += year/100 + 1 : century += year/100
-end
-puts century_from_year(124)
--- daily_excerise_2017_06_15/2017_06_15_B5_Fibonaci.rb
+++
@@ -1,6 +0,0 @@
-def fibonacci(n)
-	(n == 1 || n == 2)? fn = 1 : fn = fibonacci(n-1) + fibonacci(n-2)
-	fn
-end
-
-puts fibonacci(7)
